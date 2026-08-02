c98030aa09f82317bd0518728a7a9ff82f36e199
ErickDanielMaldonadoAguilar <erickdanielmaldonado2004@gmail.com>
2026-08-02 14:26:36 -0600

    se configuro cluster Kafka tolerante a fallos

--- consumidor/configuracion.py
+++ consumidor/configuracion.py
@@ -3,7 +3,11 @@ import os
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv(
     "KAFKA_BOOTSTRAP_SERVERS",
-    "localhost:9092",
+    (
+        "localhost:9092,"
+        "localhost:9093,"
+        "localhost:9094"
+    ),
 )
 
 KAFKA_TOPIC_LLAMADAS = os.getenv(
@@ -11,15 +15,11 @@ KAFKA_TOPIC_LLAMADAS = os.getenv(
     "llamadas-emergencia",
 )
 
-# Se utiliza para las pruebas que buscan lotes específicos
-# desde el inicio del topic.
 KAFKA_CONSUMER_GROUP_PREFIX = os.getenv(
     "KAFKA_CONSUMER_GROUP_PREFIX",
     "central911-verificacion",
 )
 
-# Grupo estable del consumidor que almacenará eventos
-# continuamente en MongoDB.
 KAFKA_CONSUMER_GROUP_PROCESAMIENTO = os.getenv(
     "KAFKA_CONSUMER_GROUP_PROCESAMIENTO",
     "central911-procesamiento-mongodb",
--- generador/app/configuracion_kafka.py
+++ generador/app/configuracion_kafka.py
@@ -1,16 +1,17 @@
 import os
 
-from generador.app.models.evento import DistritoId
+from generador.app.models.evento import (
+    DistritoId,
+)
 
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv(
     "KAFKA_BOOTSTRAP_SERVERS",
-    "localhost:9092",
-)
-
-KAFKA_TOPIC_LLAMADAS = os.getenv(
-    "KAFKA_TOPIC_LLAMADAS",
-    "llamadas-emergencia",
+    (
+        "localhost:9092,"
+        "localhost:9093,"
+        "localhost:9094"
+    ),
 )
 
 KAFKA_CLIENT_ID = os.getenv(
@@ -18,8 +19,16 @@ KAFKA_CLIENT_ID = os.getenv(
     "central911-generador",
 )
 
+KAFKA_TOPIC_LLAMADAS = os.getenv(
+    "KAFKA_TOPIC_LLAMADAS",
+    "llamadas-emergencia",
+)
 
-PARTICIONES_POR_DISTRITO: dict[DistritoId, int] = {
+
+PARTICIONES_POR_DISTRITO: dict[
+    DistritoId,
+    int,
+] = {
     DistritoId.CENTRO: 0,
     DistritoId.NORTE: 1,
     DistritoId.SUR: 2,
